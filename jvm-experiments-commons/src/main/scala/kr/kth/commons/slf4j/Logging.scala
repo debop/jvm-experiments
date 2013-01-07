a1c53b002e8c77d4a19e2f1e1528e292427661b5
debop <sunghyouk.bae@gmail.com>
2013-01-08 08:31:33 +0900

    scala 용 logging 제작 완료 ( scala 의 overloading 이 예민함 )

--- jvm-experiments-commons/src/main/scala/kr/kth/commons/slf4j/Logging.scala
+++ jvm-experiments-commons/src/main/scala/kr/kth/commons/slf4j/Logging.scala
@@ -19,16 +19,13 @@ trait Logging {
 	protected def trace(msg: String, t: Throwable) {
 		log.trace(msg, t)
 	}
-
 	protected def trace(marker: Marker, msg: String, t: Throwable) {
 		log.trace(marker, msg, t)
 	}
-
 	@varargs
 	protected def trace(format: => Any, arguments: Any*) {
 		log.trace(format, arguments)
 	}
-
 	@varargs
 	protected def trace(marker: Marker, format: => Any, arguments: Any*) {
 		log.trace(marker, format, arguments)
@@ -40,58 +37,47 @@ trait Logging {
 	protected def debug(msg: String, t: Throwable) {
 		log.debug(msg, t)
 	}
-
 	protected def debug(marker: Marker, msg: String, t: Throwable) {
 		log.debug(marker, msg, t)
 	}
-
 	@varargs
 	protected def debug(format: => Any, arguments: Any*) {
 		log.debug(format, arguments)
 	}
-
 	@varargs
 	protected def debug(marker: Marker, format: => Any, arguments: Any*) {
 		log.debug(marker, format, arguments)
 	}
-
 
 	protected lazy val isInfoEnabled = log.isInfoEnabled
 
 	protected def info(msg: String, t: Throwable) {
 		log.info(msg, t)
 	}
-
 	protected def info(marker: Marker, msg: String, t: Throwable) {
 		log.info(marker, msg, t)
 	}
-
 	@varargs
 	protected def info(format: => Any, arguments: Any*) {
 		log.info(format, arguments)
 	}
-
 	@varargs
 	protected def info(marker: Marker, format: => Any, arguments: Any*) {
 		log.info(marker, format, arguments)
 	}
-
 
 	protected lazy val isWarnEnabled = log.isWarnEnabled
 
 	protected def warn(msg: String, t: Throwable) {
 		log.warn(msg, t)
 	}
-
 	protected def warn(marker: Marker, msg: String, t: Throwable) {
 		log.warn(marker, msg, t)
 	}
-
 	@varargs
 	protected def warn(format: => Any, arguments: Any*) {
 		log.warn(format, arguments)
 	}
-
 	@varargs
 	protected def warn(marker: Marker, format: => Any, arguments: Any*) {
 		log.warn(marker, format, arguments)
@@ -102,16 +88,13 @@ trait Logging {
 	protected def error(msg: String, t: Throwable) {
 		log.error(msg, t)
 	}
-
 	protected def error(marker: Marker, msg: String, t: Throwable) {
 		log.error(marker, msg, t)
 	}
-
 	@varargs
 	protected def error(format: => Any, arguments: Any*) {
 		log.error(format, arguments)
 	}
-
 	@varargs
 	protected def error(marker: Marker, format: => Any, arguments: Any*) {
 		log.error(marker, format, arguments)
