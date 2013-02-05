7abcf745e9970142d0a2a654547aaf15dad6f064
debop <sunghyouk.bae@gmail.com>
2013-02-05 21:38:46 +0900

    hibernate-search 예제 추가

--- jvm-experiments-hibernate/src/test/java/org/hibernate/search/SearchTest.java
+++ jvm-experiments-hibernate/src/test/java/org/hibernate/search/SearchTest.java
@@ -1,9 +1,20 @@
 package org.hibernate.search;
 
 import lombok.extern.slf4j.Slf4j;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Restrictions;
+import org.hibernate.search.impl.FullTextSessionImpl;
+import org.hibernate.search.model.SearchItem;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.List;
 
 /**
  * org.hibernate.search.SearchTest
@@ -14,4 +25,36 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = "/applicationContext.xml")
 public class SearchTest {
+
+    @Autowired
+    SessionFactory sessionFactory;
+
+    private Session session;
+    private FullTextSession fullTextSession;
+
+    @Before
+    public void before() {
+        session = sessionFactory.openSession();
+        fullTextSession = new FullTextSessionImpl(session);
+    }
+
+    @Test
+    public void firstSearch() {
+
+        SearchItem item = new SearchItem();
+        item.setTitle("아이템 제목");
+        item.setDescription("아이템 설명");
+        item.setEan("USD");
+        item.setImageURL("http://debop.blogspot.com");
+
+        fullTextSession.save(item);
+        fullTextSession.flush();
+        fullTextSession.clear();
+
+        List<SearchItem> list = (List<SearchItem>)fullTextSession.createCriteria(SearchItem.class).add(Restrictions.ilike("title", "제목")).list();
+
+        Assert.assertNotNull(list);
+        for(SearchItem loaded : list)
+            System.out.println(loaded.getTitle());
+    }
 }
---
+++ jvm-experiments-hibernate/src/test/java/org/hibernate/search/model/SearchItem.java
@@ -0,0 +1,43 @@
+package org.hibernate.search.model;
+
+import kr.nsoft.data.domain.model.AnnotatedEntityBase;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.search.annotations.*;
+
+import javax.persistence.*;
+
+/**
+ * org.hibernate.search.model.SearchItem
+ * User: sunghyouk.bae@gmail.com
+ * Date: 13. 2. 5.
+ */
+@Entity
+@Indexed
+@Table(name="SEARCH_ITEM")
+public class SearchItem extends AnnotatedEntityBase {
+
+    @Id @GeneratedValue
+    @Column(name="SEARCH_ITEM_ID")
+    @DocumentId
+    private Long id;
+
+    @Getter @Setter
+    @Column(name="SEARCH_ITEM_TITLE")
+    @Field                                              // indexing with tokenization
+    private String title;
+
+    @Getter @Setter
+    @Column(name="SEARCH_ITEM_DESC")
+    @Field                                              // indexing with tokenization
+    private String description;
+
+    @Getter @Setter
+    @Column(name="SEARCH_ITEM_EAN")
+    @Field(index = Index.NO, store= Store.YES)          // indexing without tokenization
+    private String ean;
+
+    @Getter @Setter
+    @Column(name="SEARCH_ITEM_IMG")
+    private String imageURL;                            // not indexing (default)
+}
